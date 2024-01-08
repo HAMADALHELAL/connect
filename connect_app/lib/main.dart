ed75f22dbf34d3f324346a61f0e1db60fd181ea0
HAMADALHELAL <wldalhelal@hotmail.com>
2024-01-08 09:15:27 +0300

    ggez

---
+++ connect_app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:connect_app/app/config/routes/routes.dart';
+import 'package:connect_app/app/config/theme/my_theme.dart';
+import 'package:connect_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+        title: 'Flutter Demo',
+        routerConfig: AppRouter.router,
+        theme: MyTheme.lightTheme,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: const Locale('en'));
+  }
+}
